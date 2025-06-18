9e8e4b933c69cf824cbdbee437adf66b1de2445e
margadonchuk <margadonchuk@gmail.com>
2025-06-18 10:35:38 -0700

    Add Servers controller and views

---
+++ CloudCityCenter/Controllers/ServersController.cs
@@ -0,0 +1,147 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CloudCityCenter.Data;
+using CloudCityCenter.Models;
+
+namespace CloudCityCenter.Controllers;
+
+public class ServersController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ServersController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Servers
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Servers.ToListAsync());
+    }
+
+    // GET: Servers/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var server = await _context.Servers
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (server == null)
+        {
+            return NotFound();
+        }
+
+        return View(server);
+    }
+
+    // GET: Servers/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Servers/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Name,Location,PricePerMonth,Configuration,IsAvailable")] Server server)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(server);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(server);
+    }
+
+    // GET: Servers/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var server = await _context.Servers.FindAsync(id);
+        if (server == null)
+        {
+            return NotFound();
+        }
+        return View(server);
+    }
+
+    // POST: Servers/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Location,PricePerMonth,Configuration,IsAvailable")] Server server)
+    {
+        if (id != server.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(server);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ServerExists(server.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(server);
+    }
+
+    // GET: Servers/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var server = await _context.Servers
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (server == null)
+        {
+            return NotFound();
+        }
+
+        return View(server);
+    }
+
+    // POST: Servers/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var server = await _context.Servers.FindAsync(id);
+        if (server != null)
+        {
+            _context.Servers.Remove(server);
+            await _context.SaveChangesAsync();
+        }
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool ServerExists(int id)
+    {
+        return _context.Servers.Any(e => e.Id == id);
+    }
+}
