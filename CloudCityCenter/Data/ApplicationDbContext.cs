6cdd82921bd049c23b64e97ded1841a23d480724
margadonchuk <margadonchuk@gmail.com>
2025-06-18 08:41:39 -0700

    Add Server model and ApplicationDbContext

---
+++ CloudCityCenter/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CloudCityCenter.Models;
+
+namespace CloudCityCenter.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Server> Servers { get; set; } = null!;
+}
+
