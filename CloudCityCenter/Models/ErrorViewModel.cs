5288e01c8d2918f2eb25165d6fafbbebf9fbca32
IT <margadonchuk@gmail.com>
2025-06-18 08:15:13 -0700

    Initial MVC project created

---
+++ CloudCityCenter/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CloudCityCenter.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
