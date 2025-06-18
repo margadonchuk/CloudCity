726953c6683105d6a225c749b2fc655e1da42cf3
IT <margadonchuk@gmail.com>
2025-06-18 09:02:15 -0700

    Add Server model and ApplicationDbContext

---
+++ CloudCityCenter/Models/Server.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CloudCityCenter.Models;
+
+public class Server
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(100)]
+    public string Location { get; set; } = string.Empty;
+
+    [Required]
+    [Range(0, double.MaxValue)]
+    public decimal PricePerMonth { get; set; }
+
+    [Required]
+    [StringLength(200)]
+    public string Configuration { get; set; } = string.Empty;
+
+    public bool IsAvailable { get; set; }
+}
