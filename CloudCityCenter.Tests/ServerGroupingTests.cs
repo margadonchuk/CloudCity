d348137cf69f65c6bd032e509b6c4af9a4262879
margadonchuk <margadonchuk@gmail.com>
2025-06-20 07:06:58 -0700

    Group servers in services carousel

---
+++ CloudCityCenter.Tests/ServerGroupingTests.cs
@@ -0,0 +1,20 @@
+using CloudCityCenter.Models;
+using System.Linq;
+
+namespace CloudCityCenter.Tests;
+
+public class ServerGroupingTests
+{
+    [Fact]
+    public void Chunk_GroupsItemsIntoSetsOfThree()
+    {
+        var servers = Enumerable.Range(1, 7).Select(i => new Server { Id = i, Name = $"S{i}" }).ToList();
+
+        var chunks = servers.Chunk(3).ToList();
+
+        Assert.Equal(3, chunks.Count);
+        Assert.Equal(3, chunks[0].Length);
+        Assert.Equal(3, chunks[1].Length);
+        Assert.Single(chunks[2]);
+    }
+}
