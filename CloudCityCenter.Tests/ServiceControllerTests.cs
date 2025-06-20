8162573dd3dd53138d49624baa9e2e26004fd83c
margadonchuk <margadonchuk@gmail.com>
2025-06-20 10:30:39 -0700

    Add ServiceController unit tests

--- CloudCityCenter.Tests/ServiceControllerTests.cs
+++ CloudCityCenter.Tests/ServiceControllerTests.cs
@@ -1,44 +1,39 @@
 using CloudCityCenter.Controllers;
 using CloudCityCenter.Data;
 using CloudCityCenter.Models;
+using CloudCityCenter.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.EntityFrameworkCore.Storage;
 using System;
 
 namespace CloudCityCenter.Tests;
 
-public class HomeControllerTests
+public class ServiceControllerTests
 {
     [Fact]
     public async Task Index_ReturnsViewResult_WithServers()
     {
         var root = new InMemoryDatabaseRoot();
         var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-            .UseInMemoryDatabase("Home_Index_ReturnsViewResult_WithServers", root)
+            .UseInMemoryDatabase("Service_Index_ReturnsViewResult_WithServers", root)
             .Options;
         await using var context = new ApplicationDbContext(options);
         await context.Database.EnsureDeletedAsync();
         await context.Database.EnsureCreatedAsync();
-        context.Servers.Add(new Server
-        {
-            Id = 1,
-            Name = "S1",
-            Location = "US",
-            PricePerMonth = 10,
-            Configuration = "C",
-            IsAvailable = true,
-            ImageUrl = "img"
-        });
+        context.Servers.AddRange(
+            new Server { Id = 1, Name = "S1", Location = "US", PricePerMonth = 10, Configuration = "C1", IsAvailable = true, ImageUrl = "img" },
+            new Server { Id = 2, Name = "S2", Location = "EU", PricePerMonth = 20, Configuration = "C2", IsAvailable = false, ImageUrl = "img" }
+        );
         await context.SaveChangesAsync();
-        var controller = new HomeController(NullLogger<HomeController>.Instance, context);
+        var controller = new ServiceController(context);
 
         var result = await controller.Index();
 
         var viewResult = Assert.IsType<ViewResult>(result);
-        var model = Assert.IsAssignableFrom<List<Server>>(viewResult.Model);
-        Assert.Single(model);
+        var model = Assert.IsType<ServiceIndexViewModel>(viewResult.Model);
+        Assert.Equal(2, model.Servers.Count());
+        Assert.Contains(model.Servers, s => s.Name == "S1");
+        Assert.Contains(model.Servers, s => s.Name == "S2");
     }
 }
-
