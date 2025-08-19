76d90451e8624282da0aa1c3c2869101a2913ffc
margadonchuk <margadonchuk@gmail.com>
2025-08-19 04:09:29 -0700

    Add hosting page view model and product queries

--- CloudCityCenter/Controllers/HostingController.cs
+++ CloudCityCenter/Controllers/HostingController.cs
@@ -1,6 +1,57 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CloudCityCenter.Data;
+using CloudCityCenter.Models;
+using CloudCityCenter.Models.ViewModels;
+
+namespace CloudCityCenter.Controllers;
 
 public class HostingController : Controller
 {
-    public IActionResult Index() => View();
+    private readonly ApplicationDbContext _context;
+
+    public HostingController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var hostingPlans = await _context.Products
+            .Where(p => p.Type == ProductType.Hosting && p.IsPublished)
+            .Select(p => new ProductCardVm
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Slug = p.Slug,
+                Location = p.Location,
+                PricePerMonth = p.PricePerMonth,
+                Configuration = p.Configuration,
+                ImageUrl = p.ImageUrl
+            })
+            .ToListAsync();
+
+        var websiteProducts = await _context.Products
+            .Where(p => p.Type == ProductType.Website && p.IsPublished)
+            .Select(p => new ProductCardVm
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Slug = p.Slug,
+                Location = p.Location,
+                PricePerMonth = p.PricePerMonth,
+                Configuration = p.Configuration,
+                ImageUrl = p.ImageUrl
+            })
+            .ToListAsync();
+
+        var vm = new HostingPageVm
+        {
+            HostingPlans = hostingPlans,
+            WebsiteProducts = websiteProducts
+        };
+
+        return View(vm);
+    }
 }
