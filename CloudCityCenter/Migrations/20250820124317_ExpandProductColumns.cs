d93dc44f4dc8cc7aee37d7906b737f3198603ef3
master1 <master1@yourdomain.com>
2025-08-20 16:22:53 +0300

    whaT?

---
+++ CloudCityCenter/Migrations/20250820124317_ExpandProductColumns.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CloudCityCenter.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExpandProductColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
